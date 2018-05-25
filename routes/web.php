da93a05751d421e07e6fdd6470629bfeb8d64ba4
Adriel Welter <awelterramos@gmail.com>
2018-05-25 10:45:35 -0300

    Definindo routes dos Admin e Resets Passwords

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,23 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+// Admin routes
+Route::prefix('admin')->group(function(){
+    Route::get('/', 'AdminsController@index')->name('admin');
+    Route::get('login', 'Auth\AdminLoginController@showLoginForm')->name('adminLoginForm');
+    Route::post('login', 'Auth\AdminLoginController@login')->name('adminLoginSubmit');
+    Route::post('login/logout', 'Auth\AdminLoginController@logout')->name('adminLogout');
+    
+    // reset password
+    Route::post('password/email', 'Auth\AdminForgotPasswordController@sendResetLinkEmail')->name('adminResetEmail'); // admin.password.email
+    Route::get('password/reset', 'Auth\AdminForgotPasswordController@showLinkRequestForm')->name('adminResetRequestForm'); // admin.password.request
+    Route::post('password/reset', 'Auth\AdminResetPasswordController@reset')->name('resetPostRequest');
+    Route::get('password/reset/{token}', 'Auth\AdminResetPasswordController@showResetForm')->name('adminShowResetForm'); // admin.password.reset
+
+});
+
