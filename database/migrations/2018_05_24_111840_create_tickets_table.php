aa88b0d4efdb25490199c32f56c2b60c5df7fe41
Adriel Welter <awelterramos@gmail.com>
2018-05-25 10:15:46 -0300

    Criando a tabela de Migrations

---
+++ database/migrations/2018_05_24_111840_create_tickets_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('uuid');
+            $table->string('ref');
+            $table->string('title');
+            $table->string('fullname');
+            $table->string('email');
+            $table->string('category');
+            $table->string('level');
+            $table->string('status')->default('New');
+            $table->longText('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
