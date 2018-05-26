755f56cd23ff80b09fdef18cc750c5c8bd280b59
Adriel Welter <awelterramos@gmail.com>
2018-05-26 00:04:33 -0300

    Controller Comentarios dos Tickets

---
+++ app/Comment.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Ticket;
+use App\User;
+
+class Comment extends Model
+{
+    protected $guarded = [];
+
+    public function ticket()
+    {
+        return $this->belongsTo(Ticket::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+}
