8dc99a3303ede4b8d3a8d101367225f9512637b3
Adriel Welter <awelterramos@gmail.com>
2018-05-26 00:06:51 -0300

    Novas Rotas

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,10 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::resource('tickets', 'TicketsController');
+
+Route::resource('comments', 'CommentsController');
+
 // Admin routes
 Route::prefix('admin')->group(function(){
     Route::get('/', 'AdminsController@index')->name('admin');
